91163bb8923ef53316760b670e2701f1c1b98d9e
abdelrahmanadel-fci <abdelrahman.adel.fci@gmail.com>
2026-04-10 16:29:16 +0200

    create project repo

---
+++ GraphicsPackage/GraphicsPackage/DrawingAlgorithms/LineBresenham.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace GraphicsPackage
+{
+    public class BresenhamLine
+    {
+        public List<StepData> Draw(int x0, int y0, int x1, int y1, PixelCanvas canvas)
+        {
+            List<StepData> steps = new List<StepData>();
+
+            int dx = Math.Abs(x1 - x0);
+            int dy = Math.Abs(y1 - y0);
+
+            int sx = (x1 >= x0) ? 1 : -1;
+            int sy = (y1 >= y0) ? 1 : -1;
+
+            int x = x0;
+            int y = y0;
+            int k = 0;
+
+            canvas.Plot(x, y);
+
+            // CASE 1: slope <= 1
+            if (dx >= dy)
+            {
+                int p = 2 * dy - dx;
+
+                while (x != x1)
+                {
+                    x += sx;
+
+                    if (p < 0)
+                    {
+                        p += 2 * dy;
+                    }
+                    else
+                    {
+                        y += sy;
+                        p += 2 * (dy - dx);
+                    }
+
+                    canvas.Plot(x, y);
+
+                    steps.Add(new StepData
+                    {
+                        K = k++,
+                        P = p,
+                        X = x,
+                        Y = y
+                    });
+                }
+            }
+            // CASE 2: slope > 1
+            else
+            {
+                int p = 2 * dx - dy;
+
+                while (y != y1)
+                {
+                    y += sy;
+
+                    if (p < 0)
+                    {
+                        p += 2 * dx;
+                    }
+                    else
+                    {
+                        x += sx;
+                        p += 2 * (dx - dy);
+                    }
+
+                    canvas.Plot(x, y);
+
+                    steps.Add(new StepData
+                    {
+                        K = k++,
+                        P = p,
+                        X = x,
+                        Y = y
+                    });
+                }
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ GraphicsPackage/GraphicsPackage/DrawingAlgorithms/TestCanvas.cs
@@ -0,0 +1,19 @@
+﻿//using System.Collections.Generic;
+//using System.Drawing;
+
+//namespace GraphicsPackage
+//{
+//    public class TestCanvas : PixelCanvas
+//    {
+//        public List<Point> Points = new List<Point>();
+
+//        public TestCanvas() : base(Graphics.FromImage(new Bitmap(1, 1)))
+//        {
+//        }
+
+//        public new void Plot(int x, int y)
+//        {
+//            Points.Add(new Point(x, y));
+//        }
+//    }
+//}
